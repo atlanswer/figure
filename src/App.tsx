5d6f72ac58759a84a2fa75289faef09520924db0
Atlanswer <atlanswer@gmail.com>
2023-04-11 02:22:01 +0800

    dark variant of background

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,8 @@ import { Bookshelf } from "./tutorial/Bookshelf";
 const App: Component = () => {
   return (
     <div
+      id="content-div"
       class="min-h-screen w-screen flex flex-col place-content-center place-items-center gap-4"
-      style="background: fixed radial-gradient(circle, #D8D8D8 0.1rem, #F6F1F1 0.1rem) top left / 2rem 2rem;"
     >
       <Bookshelf name="Solid" />
     </div>
