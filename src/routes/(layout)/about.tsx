d4fbaed5e6b5a0397a76c90141e2133926c3a28b
Atlanswer <atlanswer@gmail.com>
2023-04-16 01:52:15 +0800

    update about

--- src/routes/(layout)/about.tsx
+++ src/routes/(layout)/about.tsx
@@ -1,7 +1,15 @@
 const About = () => (
   <div class="mx-auto max-w-screen-xl prose">
     <h1>Figure</h1>
-    <p>Create publication quality figures.</p>
+    <b>Create publication quality figures.</b>
+    <p>This project is built upon these technologies:</p>
+    <ul>
+      <li>Solid</li>
+      <li>SolidStart</li>
+      <li>UnoCSS</li>
+      <li>Plotly.js</li>
+    </ul>
+    <p>This project is hosted on Vercel.</p>
   </div>
 );
 
