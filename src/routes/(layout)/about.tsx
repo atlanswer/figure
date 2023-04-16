fcbf343a0ad3b8b39445e69970414ce732274ee4
Atlanswer <atlanswer@gmail.com>
2023-04-17 04:18:34 +0800

    update privacy

--- src/routes/(layout)/about.tsx
+++ src/routes/(layout)/about.tsx
@@ -10,7 +10,32 @@ const About = () => (
       <li>Observable Plot</li>
       <li>D3</li>
     </ul>
-    <p>This project is hosted on Vercel.</p>
+    <p>
+      This project is hosted on{" "}
+      <a href="https://vercel.com" target="_blank" rel="noreferrer">
+        Vercel
+      </a>
+      .
+    </p>
+
+    <h2>Privacy</h2>
+    <p>
+      This web app does not collect, store, or share any private data. The only
+      data it processes are those you uploaded via interactions and your system
+      theme. The latter one is stored locally in your browser.
+    </p>
+    <p>
+      Information related to you could still be processed by the host provider
+      Vercel. Check out their privacy policy{" "}
+      <a
+        href="https://vercel.com/legal/privacy-policy"
+        target="_blank"
+        rel="noreferrer"
+      >
+        here
+      </a>
+      .
+    </p>
   </div>
 );
 
