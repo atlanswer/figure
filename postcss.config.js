48cd33a67d607a5d9f3ead28b713da7818219c4c
Atlanswer <atlanswer@gmail.com>
2024-01-15 03:02:19 +0800

    add solid-devtool

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,12 @@
+// I don't want to install @type/node
+/* eslint-disable */
+// @ts-nocheck
+
 export default {
   purge: ["./index.html", "./src/**/*.{ts,tsx}"],
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
-    // I don't want to install @type/node
-    // eslint-disable-next-line
     ...(process.env.NODE_ENV === "production" ? { cssnano: {} } : {}),
   },
 };
