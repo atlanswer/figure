19fcdb36eed609c94ca8cf021e6b0b67c9c6822c
atlanswer <i@atlanswer.com>
2024-01-15 12:43:59 +0800

    theme provider

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,15 @@
 import type { Component } from "solid-js";
+import { ThemeProvider } from "./components/theme-provider.tsx";
 
 const App: Component = () => {
   return (
-    <main class="flex min-h-screen w-screen flex-col place-content-center place-items-center gap-4">
-      <p class="bg-gradient-to-tr from-blue-700 to-blue-400 bg-clip-text text-center align-middle font-sans text-6xl font-bold leading-normal text-transparent">
-        Solid Template
-      </p>
-    </main>
+    <ThemeProvider defaultTheme="system" storageKey="figure-theme">
+      <main class="flex min-h-screen w-screen flex-col place-content-center place-items-center gap-4">
+        <p class="bg-gradient-to-tr from-blue-700 to-blue-400 bg-clip-text text-center align-middle font-sans text-6xl font-bold leading-normal text-transparent">
+          Solid Template
+        </p>
+      </main>
+    </ThemeProvider>
   );
 };
 
---
+++ src/components/theme-provider.tsx
@@ -0,0 +1,67 @@
+import type { Accessor, JSX } from "solid-js";
+import {
+  createContext,
+  createRenderEffect,
+  createSignal,
+  useContext,
+} from "solid-js";
+
+type Theme = "light" | "dark" | "system";
+
+type ThemeProviderProps = {
+  children: JSX.Element;
+  defaultTheme: Theme;
+  storageKey: string;
+};
+
+type ThemeProviderState = {
+  theme: Accessor<Theme>;
+  setTheme: (theme: Theme) => void;
+};
+
+const initialState: ThemeProviderState = {
+  theme: () => "system",
+  setTheme: () => null,
+};
+
+const ThemeProviderContext = createContext<ThemeProviderState>(initialState);
+
+export function ThemeProvider(props: ThemeProviderProps) {
+  const [theme, setTheme] = createSignal<Theme>(
+    (localStorage.getItem(props.storageKey) as Theme) || props.defaultTheme,
+  );
+
+  createRenderEffect(() => {
+    const body = window.document.body;
+    body.classList.remove("light", "dark");
+
+    if (theme() == "system") {
+      const systemTheme =
+        window.matchMedia("(prefers-color-scheme: dark)").matches ?
+          "dark"
+        : "light";
+
+      body.classList.add(systemTheme);
+    } else {
+      body.classList.add(theme());
+    }
+  });
+
+  return (
+    <ThemeProviderContext.Provider
+      value={{
+        theme,
+        setTheme: (theme: Theme) => {
+          localStorage.setItem(props.storageKey, theme);
+          setTheme(theme);
+        },
+      }}
+    >
+      {props.children}
+    </ThemeProviderContext.Provider>
+  );
+}
+
+export const useTheme = () => {
+  return useContext(ThemeProviderContext);
+};
