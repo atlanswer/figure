3aa81ed0243d867c1ff8ca97133683d315651650
atlanswer <i@atlanswer.com>
2024-01-17 21:33:27 +0800

    prune unused files

--- src-old/routes/(layout)/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Gallery } from "~/components/figure/Gallery";
-
-const Index = () => (
-  <div class="mx-auto max-w-screen-xl">
-    <Gallery />
-  </div>
-);
-
-export default Index;
--- src/routes/404.tsx
+++ src/routes/404.tsx
@@ -1,12 +1,17 @@
-import { A } from "@solidjs/router";
-export default () => (
-  <main class="flex w-full flex-auto flex-col place-content-center place-items-center gap-4">
-    <p>
-      <span>404 - Not Found.</span>
-      <span> </span>
-      <A href="/" class="underline underline-offset-4">
-        Back home
-      </A>
-    </p>
-  </main>
-);
+import { A, useLocation } from "@solidjs/router";
+export default () => {
+  const location = useLocation();
+
+  return (
+    <main class="flex w-full flex-auto flex-col place-content-center place-items-center gap-4">
+      <code>{location.pathname}</code>
+      <p>
+        <span>404 - Not Found.</span>
+        <span> </span>
+        <A href="/" class="underline underline-offset-4">
+          Back home
+        </A>
+      </p>
+    </main>
+  );
+};
