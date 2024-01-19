97a23a698ce6e9d9233bb3ed1b208b7046ce45a1
atlanswer <i@atlanswer.com>
2024-01-19 23:16:10 +0800

    temp: add partytown

--- src/components/partytown.tsx
+++ src/components/partytown.tsx
@@ -1,7 +1,31 @@
-import type { PartytownProps } from "@builder.io/partytown/react";
+import {
+  partytownSnippet,
+  type PartytownConfig,
+} from "@builder.io/partytown/integration";
 import { isServer } from "solid-js/web";
 
-export const Partytown = (props: PartytownProps = {}) => {
+declare global {
+  interface Document {
+    /** If Partytown is initialized */
+    _partytown?: boolean;
+  }
+}
+
+export const Partytown = (props: PartytownConfig & { nonce: boolean }) => {
   // this check is only be done on the client, and skipped over on the server
   if (isServer) return;
+
+  if (!document.querySelector("script[data-partytown]")) {
+    // the append script to document code should only run on the client
+    // and only if the SSR'd script doesn't already exist within the document.
+    // If the SSR'd script isn't found in the document, then this
+    // must be a clientside only render. Append the partytown script
+    // to the <head>.
+    const scriptElm = document.createElement("script");
+    scriptElm.dataset.partytown = "";
+    scriptElm.innerHTML = partytownSnippet(props);
+  }
+
+  // should only append this script once per document, and is not dynamic
+  document._partytown = true;
 };
