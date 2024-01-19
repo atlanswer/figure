e4367cb083989641f30fddd98c6bc45c2b8bb2f0
atlanswer <i@atlanswer.com>
2024-01-19 16:18:25 +0800

    use three icons

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -1,5 +1,6 @@
 /* @refersh granular */
 
+import { Match, Switch } from "solid-js";
 import { useTheme } from "~/components/theme-provider";
 
 export const ThemeToggle = () => {
@@ -48,14 +49,37 @@ export const ThemeToggle = () => {
     </svg>
   );
 
+  const IconSystem = () => (
+    <svg
+      class="h-6 w-6"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="currentColor"
+      viewBox="0 0 24 24"
+    >
+      <path
+        fill-rule="evenodd"
+        d="M2.25 6a3 3 0 0 1 3-3h13.5a3 3 0 0 1 3 3v12a3 3 0 0 1-3 3H5.25a3 3 0 0 1-3-3V6Zm18 3H3.75v9a1.5 1.5 0 0 0 1.5 1.5h13.5a1.5 1.5 0 0 0 1.5-1.5V9Zm-15-3.75A.75.75 0 0 0 4.5 6v.008c0 .414.336.75.75.75h.008a.75.75 0 0 0 .75-.75V6a.75.75 0 0 0-.75-.75H5.25Zm1.5.75a.75.75 0 0 1 .75-.75h.008a.75.75 0 0 1 .75.75v.008a.75.75 0 0 1-.75.75H7.5a.75.75 0 0 1-.75-.75V6Zm3-.75A.75.75 0 0 0 9 6v.008c0 .414.336.75.75.75h.008a.75.75 0 0 0 .75-.75V6a.75.75 0 0 0-.75-.75H9.75Z"
+        clip-rule="evenodd"
+      />
+    </svg>
+  );
+
   return (
     <button
-      class="h-8 w-8 rounded p-1 hover:bg-sky-600 focus:ring dark:hover:bg-neutral-800"
+      class="h-8 w-8 rounded p-1 hover:bg-sky-600 focus:outline-none focus:ring dark:hover:bg-neutral-800"
       onClick={() => toggleTheme()}
     >
-      {theme() === "light" ?
-        <IconLight />
-      : <IconDark />}
+      <Switch>
+        <Match when={theme() === "system"}>
+          <IconSystem />
+        </Match>
+        <Match when={theme() === "light"}>
+          <IconLight />
+        </Match>
+        <Match when={theme() === "dark"}>
+          <IconDark />
+        </Match>
+      </Switch>
     </button>
   );
 };
