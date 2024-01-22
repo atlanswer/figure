0fb73b78b6d9dc3e0f74dca82ee9e77bd60b6f37
atlanswer <i@atlanswer.com>
2024-01-22 13:13:53 +0800

    fix: header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,8 +4,8 @@ import { A } from "@solidjs/router";
 import { ThemeToggle } from "~/components/theme-toggle";
 
 const Navigator = () => (
-  <nav class="flex-1 font-medium leading-loose text-black dark:text-white">
-    <span class="inline-block h-fit max-w-max sm:absolute sm:right-1/2 sm:translate-x-1/2 [&_>_:not(:first-child)]:ml-6">
+  <nav class="flex flex-1 font-medium leading-loose text-black dark:text-white sm:place-content-center">
+    <span class="inline-block [&>:not(:first-child)]:ml-6">
       <A
         href="/"
         class="inline-block border-sky-500 px-1 py-4 [&.active]:border-b-2 [&.inactive]:hover:border-b-2 [&.inactive]:hover:border-neutral-300 dark:[&.inactive]:hover:border-neutral-700"
@@ -27,6 +27,7 @@ const Navigator = () => (
 export const Header = () => (
   <header class="sticky top-0 place-content-center bg-white px-8 text-black shadow dark:border-neutral-900 dark:bg-black dark:text-white">
     <div class="mx-auto flex max-w-screen-xl place-content-between place-items-center gap-8">
+      <span class="hidden w-8 sm:inline" />
       <Navigator />
       <ThemeToggle />
     </div>
