4ddbb2fa8f53da9ed21724887f9f59d7122d5118
atlanswer <i@atlanswer.com>
2024-01-23 22:30:39 +0800

    try save fig as string

--- python/figure.py
+++ python/figure.py
@@ -1,5 +1,25 @@
-def test(x: list[int]):
-    return [n * n for n in x]
+import io
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.axes import Axes
+
+x = np.linspace(0, 2 * np.pi, 100)
+y = np.sin(x)
+
+fig, ax = plt.subplots(figsize=(3.5, 2))
+assert isinstance(ax, Axes)
+
+ax.plot(x, y)
+
+f = io.BytesIO()
+fig.savefig(f, format="svg", bbox_inches="tight")
+plt.close(fig)
+f.seek(0)
+figSvg = f.getvalue().decode()
+
+print(figSvg)
 
 
-test  # pyright: ignore[reportUnusedExpression] # noqa: B018
+# test  # pyright: ignore[reportUnusedExpression] # noqa: B018
