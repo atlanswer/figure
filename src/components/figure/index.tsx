34fcfdf3feae089c6913f95bfd21252b35dca650
Atlanswer <atlanswer@gmail.com>
2024-01-24 03:49:26 +0800

    add loaded figure

--- src/components/figure/index.tsx
+++ src/components/figure/index.tsx
@@ -54,12 +54,34 @@ export const Figure = () => {
     </div>
   );
 
+  const FigureLoaded = () => (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      class="h-6 w-6 fill-black"
+    >
+      <path
+        fill-rule="evenodd"
+        d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm13.36-1.814a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z"
+        clip-rule="evenodd"
+      />
+    </svg>
+  );
+
+  const [loadedFigure] = createResource(async () => {
+    await createFigure();
+    return (
+      <div class="flex place-content-center gap-2">
+        <FigureLoaded />
+        <span>Python loaded</span>
+      </div>
+    );
+  });
+
   return (
     <section class="grid grid-flow-col place-content-center place-items-center gap-4 py-4">
       <figure class="flex h-48 w-48 place-content-center place-items-center rounded border-t bg-neutral-50 text-black outline outline-1 outline-neutral-200">
-        <Suspense>
-          <FigureFallback />
-        </Suspense>
+        <Suspense fallback={<FigureFallback />}>{loadedFigure()}</Suspense>
       </figure>
       <Suspense fallback={<p class="animate-pulse">Loading...</p>}>
         <p>The square root of 3 is {result()}</p>
