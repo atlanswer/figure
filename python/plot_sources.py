070d1ad2dee3c9f00706aee615d80b9cb601ec55
Atlanswer <atlanswer@gmail.com>
2024-02-04 22:12:13 +0800

    source plot skeleton

--- python/plot_sources.py
+++ python/plot_sources.py
@@ -1,6 +1,19 @@
-import numpy as np
+import io
+
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d.axes3d import Axes3D
 
 
 def plot_sources():
-    ...
+    fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
+    assert isinstance(ax, Axes3D)
+
+    f = io.BytesIO()
+    fig.savefig(f, format="svg")
+    plt.close(fig)
+    f.seek(0)
+
+    return f.getvalue().decode()
+
+
+plot_sources  # pyright: ignore[reportUnusedExpression] # noqa: B018
