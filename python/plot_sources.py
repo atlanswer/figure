40bf3af81adcd2a1e2f4d4a03f7ca62ad63da0c9
atlanswer <i@atlanswer.com>
2024-02-27 14:18:08 +0800

    add theta and phi direction indicator to source preview

--- python/plot_sources.py
+++ python/plot_sources.py
@@ -1,11 +1,17 @@
-# spell-checker:words azim, mplot3d
+# spell-checker:words azim, mplot3d, xlim, ylim, zlim, verts3d, zdir
 
 import io
 import sys
-from typing import Literal, TypedDict, cast
+from typing import Any, Literal, TypedDict, cast
 
 import matplotlib.pyplot as plt
 import numpy as np
+from matplotlib.backend_bases import RendererBase
+from matplotlib.patches import Arc, FancyArrow, FancyArrowPatch
+from mpl_toolkits.mplot3d import proj3d
+from mpl_toolkits.mplot3d.art3d import (
+    pathpatch_2d_to_3d,  # pyright: ignore[reportUnknownVariableType]
+)
 from mpl_toolkits.mplot3d.axes3d import Axes3D
 
 if sys.platform != "emscripten":
@@ -20,15 +26,38 @@ class Source(TypedDict):
     phase: float
 
 
+class Arrow3D(FancyArrowPatch):
+    def __init__(
+        self,
+        xs: tuple[float, float],
+        ys: tuple[float, float],
+        zs: tuple[float, float],
+        *args: list[Any],
+        **kwargs: dict[str, Any],
+    ):
+        super().__init__((0, 0), (0, 0), *args, **kwargs)
+        self._verts3d = xs, ys, zs
+
+    def draw(self, renderer: RendererBase):
+        xs3d, ys3d, zs3d = self._verts3d
+        xs, ys, _ = proj3d.proj_transform(
+            xs3d, ys3d, zs3d, cast(Axes3D, self.axes).M
+        )
+        self.set_positions((xs[0], ys[0]), (xs[1], ys[1]))
+        super().draw(renderer)
+
+
 def plot_sources(sources: list[Source]):
-    sources = cast(list[Source], sources.to_py())  # type: ignore
+    if sys.platform != "emscripten":
+        ...
+    else:
+        sources = cast(list[Source], sources.to_py())  # type: ignore
+
+    scale_factor = 100
 
     fig, ax = plt.subplots(
         figsize=(2, 2),
-        subplot_kw={
-            "projection": "3d",
-            # "facecolor": "#AAAAAA",
-        },
+        subplot_kw={"projection": "3d"},
     )
     assert isinstance(ax, Axes3D)
 
@@ -42,8 +71,9 @@ def plot_sources(sources: list[Source]):
     for s in sources:
         theta = np.radians(s["theta"])
         phi = np.radians(s["phi"])
-        w = s["amplitude"] * np.cos(theta)
-        w2 = s["amplitude"] * np.sin(theta)
+        amplitude = s["amplitude"] * scale_factor
+        w = amplitude * np.cos(theta)
+        w2 = amplitude * np.sin(theta)
         u = w2 * np.cos(phi)
         v = w2 * np.sin(phi)
         ax.quiver(
@@ -63,7 +93,7 @@ def plot_sources(sources: list[Source]):
             w / 2 * 1.2,
             "J" if s["type"] == "E" else "M",
         )
-        max_amplitude = max(max_amplitude, s["amplitude"])
+        max_amplitude = max(max_amplitude, amplitude)
 
     ax.plot([0, max_amplitude], [0, 0], [0, 0], "k", linewidth=0.5)
     ax.plot([0, 0], [0, max_amplitude], [0, 0], "k", linewidth=0.5)
@@ -80,12 +110,77 @@ def plot_sources(sources: list[Source]):
     ax.set_zlim(-max_amplitude * 0.4, max_amplitude * 0.4)
     ax.set_axisbelow(True)
 
-    f = io.BytesIO()
-    fig.savefig(f, format="svg", bbox_inches="tight", pad_inches=0)
-    plt.close(fig)
-    f.seek(0)
+    arrow_phi = FancyArrow(
+        max_amplitude * 0.4,
+        max_amplitude * 0.65,
+        -max_amplitude * 0.05,
+        0,
+        length_includes_head=True,
+        width=max_amplitude * 0.025,
+        head_length=max_amplitude * 0.05,
+        color="k",
+    )
+    pathpatch_2d_to_3d(arrow_phi, zdir="z")
+    ax.add_artist(arrow_phi)
+    tail_phi = Arc(
+        (max_amplitude * 0.4, max_amplitude * 0.4),
+        max_amplitude * 0.5,
+        max_amplitude * 0.5,
+        theta1=0,
+        theta2=90,
+        linewidth=0.5,
+    )
+    pathpatch_2d_to_3d(tail_phi, zdir="z")
+    ax.add_artist(tail_phi)
+    ax.text(
+        max_amplitude * 0.7,
+        max_amplitude * 0.65,
+        0,
+        "$ϕ$",
+        fontsize="small",
+    )
+    arrow_theta = FancyArrow(
+        max_amplitude * 0.85,
+        max_amplitude * 0.9,
+        0,
+        -max_amplitude * 0.05,
+        length_includes_head=True,
+        width=max_amplitude * 0.025,
+        head_length=max_amplitude * 0.05,
+        color="k",
+    )
+    pathpatch_2d_to_3d(arrow_theta, zdir="x")
+    ax.add_artist(arrow_theta)
+    tail_theta = Arc(
+        (max_amplitude * 0.6, max_amplitude * 0.85),
+        max_amplitude * 0.5,
+        max_amplitude * 0.5,
+        theta1=0,
+        theta2=90,
+        linewidth=0.5,
+    )
+    pathpatch_2d_to_3d(tail_theta, zdir="x")
+    ax.add_artist(tail_theta)
+    ax.text(
+        0,
+        max_amplitude * 0.8,
+        max_amplitude * 1.05,
+        "$θ$",
+        fontsize="small",
+    )
 
-    return f.getvalue().decode()
+    if sys.platform != "emscripten":
+        ...
+    else:
+        f = io.BytesIO()
+        fig.savefig(f, format="svg", bbox_inches="tight", pad_inches=0)
+        plt.close(fig)
+        f.seek(0)
 
+        return f.getvalue().decode()
+
+
+if sys.platform != "emscripten":
+    plot_sources([Source(type="E", theta=90, phi=0, amplitude=1, phase=0)])
 
 plot_sources  # pyright: ignore[reportUnusedExpression] # noqa: B018
